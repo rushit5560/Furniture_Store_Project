24fb501a4b4b3f63ba03f5e3bda5746cd53dffd5
Rushit <90950237+rushit5560@users.noreply.github.com>
2021-12-13 17:08:14 +0530

    Product Details Api

---
+++ furniture_store/lib/Screens/product_details_screen/product_details_screen_widgets.dart
@@ -0,0 +1,239 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:furniture_store/Screens/home_screen/home_screen_widgets.dart';
+import 'package:furniture_store/common/api_url.dart';
+import 'package:furniture_store/common/common_widgets.dart';
+import 'package:furniture_store/common/custom_color.dart';
+import 'package:furniture_store/controllers/product_details_screen_controller/product_details_screen_controller.dart';
+import 'package:get/get.dart';
+
+class ProductImageSliderModule extends StatelessWidget {
+  ProductImageSliderModule({Key? key}) : super(key: key);
+  final productDetailsScreenController =
+      Get.find<ProductDetailsScreenController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider.builder(
+      itemCount:
+          productDetailsScreenController.productDetailLists[0].images.length,
+      itemBuilder: (context, index, realIndex) {
+        final imgUrl = ApiUrl.ApiMainPath +
+            '${productDetailsScreenController.productDetailLists[0].images[index]}';
+        return _buildImage(imgUrl, index);
+      },
+      options: CarouselOptions(
+          height: Get.height * 0.30,
+          autoPlay: true,
+          // autoPlayInterval: Duration(seconds: 3),
+          viewportFraction: 1,
+          onPageChanged: (index, reason) {
+            productDetailsScreenController.activeIndex.value = index;
+          }),
+    );
+  }
+
+  Widget _buildImage(String urlImage, int index) {
+    return Container(
+      height: Get.height * 0.20,
+      decoration: BoxDecoration(
+          color: Colors.grey.shade300,
+          image: DecorationImage(
+              image: NetworkImage('$urlImage'), fit: BoxFit.cover)),
+    );
+  }
+}
+
+class ProductImageBannerIndicator extends StatelessWidget {
+  ProductImageBannerIndicator({Key? key}) : super(key: key);
+  final productDetailsScreenController =
+      Get.find<ProductDetailsScreenController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(
+          productDetailsScreenController.productDetailLists[0].images.length,
+          (index) => Container(
+            margin: EdgeInsets.all(4),
+            width: 11,
+            height: 11,
+            decoration: BoxDecoration(
+              color: productDetailsScreenController.activeIndex.value == index
+                  ? CustomColor.kLightGreenColor
+                  : Colors.grey,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FavouriteButton extends StatelessWidget {
+  const FavouriteButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => print('Clk'),
+      child: Material(
+        borderRadius: BorderRadius.circular(40),
+        elevation: 10,
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Icon(
+              Icons.favorite_rounded,
+              color: CustomColor.kLightGreenColor,
+              size: 18,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductDetails extends StatelessWidget {
+  ProductDetails({Key? key}) : super(key: key);
+  final productDetailsScreenController =
+  Get.find<ProductDetailsScreenController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SpacerHeight(12),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Text(
+                  '${productDetailsScreenController.productDetailLists[0].productname}',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+              ),
+              Text(
+                '\$${productDetailsScreenController.productDetailLists[0]..productcost}',
+                style: TextStyle(
+                  color: CustomColor.kLightGreenColor,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+          const SpacerHeight(8),
+          Row(
+            children: [
+              Icon(Icons.star_rounded, color: Colors.yellowAccent),
+              const SizedBox(width: 10),
+              Text('4.5 '),
+              Text(
+                'review',
+                style: TextStyle(decoration: TextDecoration.underline),
+              ),
+            ],
+          ),
+          const SpacerHeight(8),
+          CustomDivider(),
+          TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(text: 'Description'),
+              Tab(text: 'review'),
+            ],
+          ),
+          CustomDivider(),
+          Container(
+            height: Get.height * 0.20,
+            child: TabBarView(
+              children: [
+                _productDescription(),
+                _productDescription(),
+              ],
+            ),
+          ),
+          NewArrivalModule(),
+          _cartAndBuyButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _productDescription() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        '${productDetailsScreenController.productDetailLists[0].fullText}',
+        maxLines: 5,
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+
+  Widget _cartAndBuyButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: Get.width * 0.30,
+              color: CustomColor.kLightGreenColor,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Add To Cart',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: Get.width * 0.30,
+              color: CustomColor.kLightGreenColor,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Buy',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- furniture_store/lib/controllers/product_details_screen_controller/product_details_screen_controller.dart
+++ furniture_store/lib/controllers/product_details_screen_controller/product_details_screen_controller.dart
@@ -1,11 +1,51 @@
-import 'package:flutter/material.dart';
-import 'package:furniture_store/common/image_url.dart';
+import 'dart:convert';
+
+import 'package:furniture_store/common/api_url.dart';
+import 'package:furniture_store/models/product_detail_screen_model/product_detail_model.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class ProductDetailsScreenController extends GetxController {
-
+  int productId = Get.arguments;
+  RxBool isLoading = false.obs;
+  RxBool isStatus = false.obs;
   RxInt activeIndex = 0.obs;
-  List<String> productImgList = [ImageUrl.product1, ImageUrl.product2, ImageUrl.product3];
+  RxList<Datum> productDetailLists = RxList();
 
 
+  getProductDetailData() async {
+    isLoading(true);
+    String url = ApiUrl.ProductDetailApi;
+    print('Url : $url');
+
+    try{
+      Map data = {
+        "id": "$productId"
+      };
+
+      http.Response response = await http.post(Uri.parse(url), body: data);
+
+      ProductDetailsData productDetailsData = ProductDetailsData.fromJson(json.decode(response.body));
+      isStatus = productDetailsData.success.obs;
+
+      if(isStatus.value){
+        productDetailLists = productDetailsData.data.obs;
+      } else {
+        print('Product Details False False');
+      }
+
+    } catch(e) {
+      print('Product Details Error : $e');
+    } finally {
+      isLoading(false);
+    }
+    // getProductReview();
+  }
+
+
+  @override
+  void onInit() {
+    getProductDetailData();
+    super.onInit();
+  }
 }
